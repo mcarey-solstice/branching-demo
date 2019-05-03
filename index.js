7751160a90abd343359861fd5989c7eac5f8ace0
Mike Carey <mcarey@solstice.com>
2019-05-02 19:09:44 -0500

    Adds a / endpoint

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const version = fs.readFileSync('VERSION').toString().trim()
 
 let app = express()
 
+app.get('/', function (req, res, next) {
+  return res.send({"message": "hello world"})
+})
+
 app.get('/version', function(req, res, next) {
   return res.send({"version": version})
 });
